e4061b9364379829654f826d0737350814f1d913
BWeesy <Benedictwesson@gmail.com>
2019-07-11 22:32:25 +0100

    Add notes to be fleshed out later

--- NextFrame.cs
+++ NextFrame.cs
@@ -12,20 +12,21 @@ namespace SwarmSim
 {
     public static class NewFrame
     {
-        [FunctionName("NewFrame")]
-        public static async Task<HttpResponseMessage> Run(
+        [FunctionName("NextFrame")]
+        public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            Frame frame = new Frame();
-            frame.Init();
-            log.LogInformation($"{frame.ToString()}");
-
-            var responseJson = JsonConvert.SerializeObject(frame.map);
-
-            return new HttpResponseMessage(HttpStatusCode.OK) {
-                Content = new StringContent(responseJson, Encoding.UTF8, "application/json")
-            };
+            //Read request body
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //Turn data into a Frame
+            //Validate is a valid Frame
+            //Create next frame from incoming frame
+            //Return created frame
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
 }
