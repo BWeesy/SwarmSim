44f44bff163f4fc6da36d55c291a938336e6ca6b
Ben Wesson <Ben.Wesson@3squared.com>
2019-07-17 14:18:54 +0100

    Flesh out MoveDrone and start finding unexplored spaces to move to

--- Classes/Drone.cs
+++ Classes/Drone.cs
@@ -1,3 +1,4 @@
+using System;
 using SwarmSim.Enums;
 using SwarmSim.Interfaces;
 
@@ -27,18 +28,26 @@ namespace SwarmSim.Classes.Entities
         public static ISpace[,] UngroupedDroneMove(int x, int y, Drone drone, ISpace[,] previousMap)
         {
             var map = previousMap;
+            //TODO Look for close Leaders with space in their group. If found, move towards them
+            //Else, explore by moving towards closest unexplored space
+            var targetSpace = FindAdjacentUnexplored(x, y, map);
+
+            map = MoveDrone(x, y, targetSpace.x, targetSpace.y, map);
+
             return map;
         }
 
         public static ISpace[,] LeaderDroneMove(int x, int y, Drone drone, ISpace[,] previousMap)
         {
             var map = previousMap;
+            //Explore by moving towards closest unexplored space
             return map;
         }
 
         public static ISpace[,] SubordinateDroneMove(int x, int y, Drone drone, ISpace[,] previousMap)
         {
             var map = previousMap;
+            //Follow the leader
             return map;
         }
 
@@ -46,15 +55,29 @@ namespace SwarmSim.Classes.Entities
         {
             var map = previousMap;
             //Validate currentCoords are actually a drone
-
+            if (!(map[currentX, currentY] is Drone))
+            {
+               throw new Exception($"Failed to move drone from {map[currentX, currentY]} as it is not a drone");
+            }
             //Validate target is not solid
+            if (map[targetX, targetY].IsSolid())
+            {
+               throw new Exception($"Failed to move drone into {map[targetX, targetY]} as that space is solid");
+            }
 
             //Move drone into the target space
+            map[targetX, targetY] = map[currentX, currentY];
 
             //Change old space to an Explored space
+            map[currentX, currentY] = new Explored();
+
             return map;
         }
-        //TODO Move Drone Method
+
+        private static (x, y) FindAdjacentUnexplored(int x, int y, ISpace[,] map)
+        {
+
+        }
         //TODO Patchfinding method
         //TODO Explore method
             //Look for closest unexplored
